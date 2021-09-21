a5db616cdd1f77ae0de883bed3ce62ddffd0747e
patyspro <patyspro@gmail.com>
2021-09-21 18:22:53 -0300

    adicionando o tema dark

---
+++ js/tarefas.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body');
+
+let buttomTema = document.querySelector('#tema');
+buttomTema.addEventListener("click", function(){
+    body.classList.toggle('dark');
+});
+
