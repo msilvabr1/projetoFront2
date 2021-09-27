7925a06635cd6840e337866352b4072965292fcb
Adriano Viera dos Santos <adrianovs.info@gmail.com>
2021-09-27 11:51:23 -0300

    all cards and remove all cards buttons

--- js/cadastroTarefas.js
+++ js/cadastroTarefas.js
@@ -3,13 +3,18 @@ let count = 0;
 const api = fetch('https://jsonplaceholder.typicode.com/todos/');
 
 let goahead = _ =>{
-  if (count<0){count=0;}
+  
+
 produto=api.then(res => res.clone().json()).then(json => {
+  var obj = JSON.parse(JSON.stringify(json));
+  var length = Object.keys(obj).length;
   const body = document.querySelector("body")
   body.innerHTML = 
   `<script src="./js/cadastroTarefas.js"></script>
   <button type="submit" class="btn" id="enviar" onclick="goahead();">Card Push Ahead</button>
   <button type="submit" class="btn" id="enviar" onclick="goback();">Card Pull Back</button>
+  <button type="submit" class="btn" id="enviar" onclick="allcards();">All Cards at once</button>
+  <button type="submit" class="btn" id="enviar" onclick="removecards();">Remove All Cards</button>
   <br>
   `;
   json.map(produto => {
@@ -44,19 +49,20 @@ produto=api.then(res => res.clone().json()).then(json => {
         body.innerHTML += card;
     }
   });
+  if(count<length)count++
 });
-count++;
 }
 
 
 let goback = _ =>{
-  if (count<0){count=0;}
   produto=api.then(res => res.clone().json()).then(json => {
     const body = document.querySelector("body")
     body.innerHTML = 
     `<script src="./js/cadastroTarefas.js"></script>
     <button type="submit" class="btn" id="enviar" onclick="goahead();">Card Push Ahead</button>
     <button type="submit" class="btn" id="enviar" onclick="goback();">Card Pull Back</button>
+    <button type="submit" class="btn" id="enviar" onclick="allcards();">All Cards at once</button>
+    <button type="submit" class="btn" id="enviar" onclick="removecards();">Remove All Cards</button>
     <br>
     `;
     json.map(produto => {
@@ -92,6 +98,68 @@ let goback = _ =>{
       }
     });
   });
-  count--;
+  if (count==0){count=0;}else count--;
   }
-  
+
+
+  let allcards = _ =>{
+    produto=api.then(res => res.clone().json()).then(json => {
+      const body = document.querySelector("body")
+      body.innerHTML = 
+      `<script src="./js/cadastroTarefas.js"></script>
+      <button type="submit" class="btn" id="enviar" onclick="goahead();">Card Push Ahead</button>
+      <button type="submit" class="btn" id="enviar" onclick="goback();">Card Pull Back</button>
+      <button type="submit" class="btn" id="enviar" onclick="allcards();">All Cards at once</button>
+      <button type="submit" class="btn" id="enviar" onclick="removecards();">Remove All Cards</button>
+      <br>
+      `;
+      json.map(produto => {
+        if(produto.completed&&produto.id){
+        let card = `
+          <div class="card2">
+            <img src="./assets/img_avatar2.jpg" alt="Avatar" style="width:100%">
+            <div class="container2">
+              <h3>${produto.userId}</h3>
+              <strong>${produto.id}</strong>
+              <p><del>${produto.title}</del></p>
+              <p>${produto.completed}</p>
+            </div>
+          </div>
+        `;
+        
+        body.innerHTML += card;
+        }
+          if(!produto.completed&&produto.id){
+            let card = `
+            <div class="card2">
+            <img src="./assets/img_avatar.jpg" alt="Avatar" style="width:100%">
+            <div class="container2">
+              <h3>${produto.userId}</h3>
+              <strong>${produto.id}</strong>
+              <p><strong>${produto.title}</strong></p>
+              <p>${produto.completed}</p>
+            </div>
+          </div>
+            `;
+            
+            body.innerHTML += card;
+        }
+      });
+      count=200;
+    });
+    }
+
+    let removecards = _ =>{
+        const body = document.querySelector("body")
+        body.innerHTML = 
+        `<script src="./js/cadastroTarefas.js"></script>
+        <button type="submit" class="btn" id="enviar" onclick="goahead();">Card Push Ahead</button>
+        <button type="submit" class="btn" id="enviar" onclick="goback();">Card Pull Back</button>
+        <button type="submit" class="btn" id="enviar" onclick="allcards();">All Cards at once</button>
+        <button type="submit" class="btn" id="enviar" onclick="removecards();">Remove All Cards</button>
+        <br>
+        `;
+        count=0;
+      }
+    
+    
