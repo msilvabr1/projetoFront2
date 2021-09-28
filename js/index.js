1862a60a583d535e65b944d5605e09173119b54f
msilvabr1 <85535170+msilvabr1@users.noreply.github.com>
2021-09-28 10:16:02 -0300

    Códigos comprimidos para melhor desempenho

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-//verificacao de igualdade do campo senha no cadastro de conta
-
-let senha = document.getElementById("senha")
-let confirmaSenha = document.getElementById("senha1");
-
-function validatePassword(){
-  if(senha.value != confirmaSenha.value) {
-    confirmaSenha.setCustomValidity("Senhas diferentes!");
-  } else {
-    confirmaSenha.setCustomValidity('');
-  }
-}
-
-senha.onchange = validatePassword;
-confirmaSenha.onkeyup = validatePassword;
+let senha=document.getElementById("senha"),confirmaSenha=document.getElementById("senha1");function validatePassword(){senha.value!=confirmaSenha.value?confirmaSenha.setCustomValidity("Senhas diferentes!"):confirmaSenha.setCustomValidity("")}senha.onchange=validatePassword,confirmaSenha.onkeyup=validatePassword;
